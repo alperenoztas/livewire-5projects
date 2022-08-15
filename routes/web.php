0ca6a285313bbf64135b2028d3e386a0364c1235
Alperen Öztaş <alprnoztas@gmail.com>
2022-08-15 11:40:38 +0300

    Todo

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Livewire\Calculator;
 use App\Http\Livewire\Counter;
+use App\Http\Livewire\TodoList;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,5 @@ Route::get('/', function () {
 });
 Route::get('/counter', Counter::class)->name('counter');
 Route::get('/calculator', Calculator::class)->name('calculator');
+ROute::get('/todo-list',TodoList::class)->name('todo-list');
 
